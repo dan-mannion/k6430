dd9bf7fe48fc20c885fcd3e506ffa14ac04486a7
dan-mannion <danjohnmannion@gmail.com>
2018-12-08 11:18:44 +0000

    Modified to use variables and not hardcoded

--- smkLinearSweepSample.m
+++ smkLinearSweepSample.m
@@ -1,7 +1,12 @@
 function output = smkLinearSweepSample(vstart, vstop, vinc, icc)
 
 k = smkConnect();
-output=smkLinearSweep(k,vstart, vstop, vinc, icc);
+data=smkLinearSweep(k,vstart, vstop, vinc, icc);
 smkDisconnect(k);
+
+%Package data into struct
+output.voltage = data(:,1);
+output.current = data(:,2);
+output.time = data(:,4);
 
 end
